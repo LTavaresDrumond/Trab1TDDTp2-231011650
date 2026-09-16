09f8cae6ecaea134056b0f2bb5ab15d49ea036db
Lucas Tavares <146688988+LTavaresDrumond@users.noreply.github.com>
2026-09-16 12:16:56 +0000

    feat: detecta vitorias de X e O

--- TDD_velha/velha.cpp
+++ TDD_velha/velha.cpp
@@ -12,8 +12,66 @@
  *  Descrever o que a funcao faz
  */
 
+static bool TemVitoria(const int velha[3][3], int jogador) {
+    for (int linha = 0; linha < 3; ++linha) {
+        if (velha[linha][0] == jogador &&
+            velha[linha][1] == jogador &&
+            velha[linha][2] == jogador) {
+            return true;
+        }
+    }
+
+    for (int coluna = 0; coluna < 3; ++coluna) {
+        if (velha[0][coluna] == jogador &&
+            velha[1][coluna] == jogador &&
+            velha[2][coluna] == jogador) {
+            return true;
+        }
+    }
+
+    return (velha[0][0] == jogador && velha[1][1] == jogador &&
+            velha[2][2] == jogador) ||
+           (velha[0][2] == jogador && velha[1][1] == jogador &&
+            velha[2][0] == jogador);
+}
+
 int VerificaVelha(int velha[3][3]) {
-    return 1;
+    int quantidadeX = 0;
+    int quantidadeO = 0;
+
+    for (int linha = 0; linha < 3; ++linha) {
+        for (int coluna = 0; coluna < 3; ++coluna) {
+            if (velha[linha][coluna] == 1) {
+                ++quantidadeX;
+            } else if (velha[linha][coluna] == 2) {
+                ++quantidadeO;
+            } else if (velha[linha][coluna] != 0) {
+                return -2;
+            }
+        }
+    }
+
+    if ((quantidadeX == 9 && quantidadeO == 0) ||
+        (quantidadeO == 9 && quantidadeX == 0)) {
+        return -2;
+    }
+
+    bool vitoriaX = TemVitoria(velha, 1);
+    bool vitoriaO = TemVitoria(velha, 2);
+
+    if (vitoriaX && vitoriaO) {
+        return -2;
+    }
+    if (vitoriaX) {
+        return 1;
+    }
+    if (vitoriaO) {
+        return 2;
+    }
+    if (quantidadeX + quantidadeO == 9) {
+        return 0;
+    }
+    return -1;
 }
 
 
